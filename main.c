4a258e7f33c0b446e824b1363e5395da2be3d1d3
Arvi-H <arvihaxhillari1@gmail.com>
2022-03-29 11:03:59 -0600

    Finished Problem 1

--- main.c
+++ main.c
@@ -2,18 +2,31 @@
     decode2:
         subq %rdx, %rsi     -> y -= z
         imulq %rsi, %rdi    -> x *= y
-        movq %rsi, %rax     -> 
-        salq $63, %rax
-        sarq $63, %rax
-        xor %rdi, %rax
-        ret
+        movq %rsi, %rax     -> a = y
+        salq $63, %rax      -> a =<< 63
+        sarq $63, %rax      -> a =>> 63
+        xor %rdi, %rax      -> a ^= x
+        ret                 -> return a
 */
 
-// x in %rdi, y in %rsi, z in %rdx
+// x in %rdi, y in %rsi, z in %rdx, a in %rax
 
 long decode2 (long x, long y, long z) {
+    y -= z;
+    x *= y;
 
+    long a = y;
+
+    a <<= 63;
+    a >>= 63;
+    a ^= x;
+    
+    return a;
 }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+
 
 
 
